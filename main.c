378e789bfbe678c80653252a186ddd62e8d9e958
Alecoow <alecoow@gmail.com>
2025-02-13 23:59:24 -0800

    Refactor

--- main.c
+++ main.c
@@ -7,39 +7,58 @@
 
 #define F_CPU 16000000UL // 16MHz clock from the debug processor
 #define BAUD 9600
-#define BAUDRATE ((F_CPU) / (BAUD*16UL)-1)
+#define BAUDRATE (F_CPU / (BAUD * 16UL) - 1)
+#include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
-
-void UART_Init (void)
-{
-	UBRR0H = (BAUDRATE>>8); // shift the register right by 8 bits
-	UBRR0L = BAUDRATE; // set baud rate
-	UCSR0B|= (1<<TXEN0)|(1<<RXEN0); // enable receiver and transmitter
-	UCSR0C|= (1<<UCSZ00)|(1<<UCSZ01); // 8bit data format
+ 
+void USART_Init(uint32_t ubrr) {
+	// Set baud rate (high and low)
+	UBRR0H = (uint8_t)(ubrr >> 8);
+	UBRR0L = (uint8_t)ubrr;
+	// Enable receive and transfer on USART0
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+	// 8bit Data
+	UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);
+	// Disable parity
+	UCSR0C &= ~((1 << UPM01) ^ (1 << UPM00));
+	// 1bit Stop
+	UCSR0C &= ~(1 << USBS0);
 }
-
-void UART_Transmit (unsigned char data) {
-	while (!( UCSR0A & (1<<UDRE0))); // wait while register is free
-	UDR0 = data; // load data in the register
+void USART_Transmit(uint8_t data) {
+	/* Wait for empty transmit buffer */
+	while (!(UCSR0A & (1 << UDRE0)))
+	{}
+	/* Put data into buffer, sends the data */
+	UDR0 = data;
 }
-// function to receive data
-unsigned char UART_Receive (void)
-{
-	while(!(UCSR0A) & (1<<RXC0)); // wait while data is being received
-	return UDR0; // return 8-bit data
+ 
+uint8_t USART_Receive() {
+	/* Wait for data to be received */
+	while (!(UCSR0A & (1 << RXC0)))
+	{}
+	/* Get and return received data from buffer */
+	return UDR0;
 }
-
-int main(void)
-{
-    const char* data = "1310748";
-	UART_Init();
-    while (1) 
-    {
-		for(int i = 0; i < 8; i++) {
-			while(!(UCSR0A & UDRE0));
-			UART_Transmit(data[i]);
-		}
-    }
+ 
+void USART_Send(const char* str) {
+	while (*str != '\0')
+	{
+		USART_Transmit(*str);
+		str++;
+	}
 }
+ 
+ 
+int main() {
+	USART_Init(BAUDRATE);
 
+	while (1)
+	{
+		USART_Send("1234567890\n");
+		_delay_ms(600);
+
+	}
+
+	return 0;
+}
